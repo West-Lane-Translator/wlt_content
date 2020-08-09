02a608c368fe785cf4c0b3795eee15643be40b4d
saagit <55336016+saagit@users.noreply.github.com>
2020-08-08 17:49:58 -0700

    Eliminate using separate publishconf.py
    To get absolute links, it was needed to use SITEURL but with the way
    the import was done, pelicanconf.py did not have access to what
    publishconf.py set it to.  I made publishconf.py just a symlink to
    pelicanconf.py and then determine the right value by examining
    __file__.  Yes, it is hacky.
    
    This commit also has extensive changes to pelicanconf.py to organize
    and document it better.  Finally, EXTRA_PATH_METADATA is now generated
    automatically.

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,53 +2,52 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 import datetime
+import inspect
+import pathlib
 
-SITEURL = 'http://localhost:8000'  # publishconf.py overrides
-AUTHOR = 'West Lane Translator Inc.'
+# The Pelican settings are documented here:
+#   https://docs.getpelican.com/en/stable/settings.html
+# and the Flex theme's settings are documented here:
+#   https://github.com/alexandrevicenzi/Flex/wiki/Custom-Settings
+
+# I personally found it a pain to have a separate pelicanconf.py and
+# publishconf.py mainly because it wasn't possible to use SITEURL in other
+# things such as FAVICON and SITELOGO.  I instead just symlink pelicanconf.py
+# to publishconf.py and use this little bit of ugliness to set a variable so
+# the code can figure out if it is publishing or not:
+PUBLISHING = __file__.endswith('publishconf.py')
+
+# Pelican general site settings
+SITEURL = 'https://westlanetv.org' if PUBLISHING else 'http://localhost:8000'
 SITENAME = 'West Lane Translator Inc.'
+TIMEZONE = 'America/Los_Angeles'
+DEFAULT_LANG = 'en'
+
+# The Flex theme can accommodate our non-blog site well and is MIT license
+THEME = '../Flex'
+
+# Flex general site settings
 SITETITLE = SITENAME
 SITESUBTITLE = 'Serving the Central Oregon Coast since 1959'
-SITELOGO = '/images/logo.png'
-SITEDESCRIPTION = (
+SITEDESCRIPTION = \
     "Information about West Lane Translator's HDTV and FM translators."
-)
+AUTHOR = 'West Lane Translator Inc.'
 COPYRIGHT_NAME = AUTHOR
 COPYRIGHT_YEAR = datetime.datetime.today().year
-TIMEZONE = 'America/Los_Angeles'
+ROBOTS = 'index, follow'
+FAVICON = SITEURL + '/images/favicon.ico'
+SITELOGO = SITEURL + '/images/logo.png'
+# Flex Dark Mode settings per:
+#   https://github.com/alexandrevicenzi/Flex/wiki/Dark-Mode
+THEME_COLOR = 'light'
+THEME_COLOR_AUTO_DETECT_BROWSER_PREFERENCE = True
+THEME_COLOR_ENABLE_USER_OVERRIDE = True  # Uses JavaScript
 
-PATH = 'content'
-PAGE_PATHS = ['pages']
-PAGE_URL = '{slug}.html'
-PAGE_SAVE_AS = '{slug}.html'
-STATIC_PATHS = ['images', 'pdfs', 'extra']
-DIRECT_TEMPLATES = []
-ARTICLE_PATHS = []
-ARCHIVES_SAVE_AS = ''
-ARTICLE_SAVE_AS = ''
-AUTHOR_SAVE_AS = ''
-CATEGORY_SAVE_AS = ''
-DRAFT_SAVE_AS = ''
-INDEX_SAVE_AS = ''
-TAG_SAVE_AS = ''
-OUTPUT_PATH = '../wlt_output/'
-
-# The site is small, so ensure output is de-crufted.
-DELETE_OUTPUT_DIRECTORY = False
-CACHE_CONTENT = False
-LOAD_CONTENT_CACHE = False
-
-DEFAULT_LANG = 'en'
-DEFAULT_PAGINATION = False
-DISPLAY_PAGES_ON_MENU = True
-DISPLAY_CATEGORIES_ON_MENU = False
-
-# We don't need any of this
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
-CATEGORY_FEED_ATOM = None
-FEED_ALL_ATOM = None
-TRANSLATION_FEED_ATOM = None
-
+# Enable some Markdown extensions.  The list of available extensions and
+# documentation for them is available here:
+#   https://python-markdown.github.io/extensions/
+# For completeness, the basic Markdown syntax is documented here:
+#   https://daringfireball.net/projects/markdown/syntax
 MARKDOWN = {
     'extension_configs': {
         'markdown.extensions.attr_list': {},
@@ -60,48 +59,87 @@ MARKDOWN = {
     'output_format': 'html5',
 }
 
-THEME = '../Flex'
+# Tell Pelican where things come from and go to.
+PATH = 'content'  # Keep content separate from how to build.
+PAGE_PATHS = ['pages']  # Where to look in content subdirectory for our pages.
+STATIC_PATHS = ['images', 'pdfs', 'extra']  # Copy things from these to output.
+OUTPUT_PATH = '../wlt_output/'  # Where to put the output.
+PAGE_URL = '{slug}.html'  # Output the HTML at the root instead of in pages/
+PAGE_SAVE_AS = '{slug}.html'
+
+# Configuring the menus is currently somewhat hacky.  The menu at the top of
+# the page is enabled with MAIN_MENU and defined with MENUITEMS:
 MAIN_MENU = True
 MENUITEMS = (
-    ( 'History', '/history.html' ),
-    ( 'Governance', '/governance.html' ),
-    ( 'Leasing', '/leasing.html' ),
-    ( 'Membership', '/membership.html' ),
-    ( 'Contact Us', '/contact-us.html' )
+    ( 'History', SITEURL + '/history.html' ),
+    ( 'Governance', SITEURL + '/governance.html' ),
+    ( 'Leasing', SITEURL + '/leasing.html' ),
+    ( 'Membership', SITEURL + '/membership.html' ),
+    ( 'Contact Us', SITEURL + '/contact-us.html' )
 )
-FOOTERMENUITEMS = (
-    ( 'Terms and Conditions', '/terms-and-conditions.html' ),
-    ( 'Privacy Policy', '/privacy-policy.html' )
-)
-ALT_CREDIT=''
-ROBOTS = 'index, follow'
+# The left side menu contains all the Markdown pages except for ones that have
+# 'Status: Hidden' attribute in their metadata.  This means that any page that
+# appears in the top or bottom menu should have that attribute in their
+# Markdown so they don't appear in the left side menu as well.  To get the
+# menu items in the preferred order, the new custom Menu attribute was put in
+# the Markdown files.  Tell Flex to sort by that new attribute:
 PAGES_SORT_ATTRIBUTE = 'menu'
-FAVICON = '/images/favicon.ico'
-THEME_COLOR = 'light'
-THEME_COLOR_AUTO_DETECT_BROWSER_PREFERENCE = True
-THEME_COLOR_ENABLE_USER_OVERRIDE = True # Uses JavaScript
+# The menu at the bottom of the page is enabled with local changes to Flex:
+FOOTERMENUITEMS = (
+    ( 'Terms and Conditions', SITEURL + '/terms-and-conditions.html' ),
+    ( 'Privacy Policy', SITEURL + '/privacy-policy.html' )
+)
 
+# I don't feel compelled to tell the world our site was built with
+# Pelican and Flex.  This relies on local changes to Flex.
+ALT_CREDIT=''
+
+# Tell the world via the JSON-LD and Open Graph types that we are a
+# website and not a blog.  These rely on local changes to Flex.
 OG_TYPE = 'website'
 JSONLD_TYPE = 'WebPage'
 
-EXTRA_PATH_METADATA = {
-    'extra/htaccess': {'path': '.htaccess'},
-    'extra/htaccess_About': {'path': 'About/.htaccess'},
-    'extra/htaccess_Donate': {'path': 'Donate/.htaccess'},
-    'extra/htaccess_EAS': {'path': 'EAS/.htaccess'},
-    'extra/htaccess_FAQ': {'path': 'FAQ/.htaccess'},
-    'extra/htaccess_FM': {'path': 'FM/.htaccess'},
-    'extra/htaccess_HDTV': {'path': 'HDTV/.htaccess'},
-    'extra/htaccess_ISS': {'path': 'ISS/.htaccess'},
-    'extra/htaccess_Introduction': {'path': 'Introduction/.htaccess'},
-    'extra/htaccess_Tides': {'path': 'Tides/.htaccess'},
-    'extra/htaccess_Tsunami': {'path': 'Tsunami/.htaccess'},
-    'extra/htaccess_WX': {'path': 'WX/.htaccess'},
-    'extra/htaccess_WXDisc': {'path': 'WXDisc/.htaccess'},
-    'extra/htaccess_Webcam': {'path': 'Webcam/.htaccess'},
-    'extra/htaccess_Webcams': {'path': 'Webcams/.htaccess'},
-    'extra/htaccess_contact': {'path': 'contact/.htaccess'},
-    'extra/htaccess_files': {'path': 'files/.htaccess'},
-    'extra/htaccess_pages': {'path': 'pages/.htaccess'},
-    'extra/htaccess_search': {'path': 'search/.htaccess'},
-}
+# This is a simple website and not a blog so disable all the bloggy stuff
+DIRECT_TEMPLATES = []
+ARCHIVES_SAVE_AS = ''
+ARTICLE_PATHS = []
+ARTICLE_SAVE_AS = ''
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+AUTHOR_SAVE_AS = ''
+CATEGORY_FEED_ATOM = None
+CATEGORY_SAVE_AS = ''
+DEFAULT_PAGINATION = False
+DISPLAY_CATEGORIES_ON_MENU = False
+DRAFT_SAVE_AS = ''
+FEED_ALL_ATOM = None
+INDEX_SAVE_AS = ''
+TAG_SAVE_AS = ''
+TRANSLATION_FEED_ATOM = None
+
+# The site is small, so rebuild it from scratch every time.
+DELETE_OUTPUT_DIRECTORY = False
+CACHE_CONTENT = False
+LOAD_CONTENT_CACHE = False
+
+# We want redirects of URLs from the old site to go to the new site
+# URLs.  This is done with .htaccess files in subdirectories on the
+# website as needed.  The source for these files is
+# content/extra/htaccess* but we need Pelican to copy them to the
+# right place in the output.  This is done via EXTRA_PATH_METADATA.
+# Instead of building that manually, the following code automatically
+# builds EXTRA_PATH_METADATA from the list of extra/htaccess* files.
+# As an example, extra/htaccess_About will end up in About/.htaccess
+# in the output.
+EXTRA_PATH_METADATA = {}
+path_path = pathlib.Path(PATH)
+extra_path = path_path / "extra"
+for hta in extra_path.glob('htaccess*'):
+    rel_hta = pathlib.Path(*hta.parts[-2:])
+    hta_loc = hta.name[len('htaccess'):]
+    if not hta_loc:
+        hta_loc = '.htaccess'
+    else:
+        assert hta_loc[0] == '_'
+        hta_loc = hta_loc[1:] + '/.htaccess'
+    EXTRA_PATH_METADATA[str(rel_hta)] = {'path': hta_loc}
